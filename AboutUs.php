2c62e47267f7afc133df4a489b49d8d7e103c3dc
Kong Wen Khang <w.kong70@yahoo.com>
2025-06-11 15:43:53 +0800

    about us and register

--- AboutUs.php
+++ AboutUs.php
@@ -17,13 +17,13 @@ include 'reshead.php';
                     <div class="intro_image"><img src="img_logo/LYL_parsonal_photo.jpg"></div>
                     <div class="intro_content">
                         <h2><b>HI</b>,Nice to Meet you.</h2>
-                        <p>I am the leader of this e-learning platform.Hope that you can have a nice experience with our platform.</p>
+                        <p>I am the developer of this e-learning platform and we want you to have a wonderful and enriching experience.</p>
                     </div>
                 </div>
                 <div class="intro_box">
                     <div class="intro_content">
                         <h2><b>HI</b>,Nice to Meet you.</h2>
-                        <p>I am the leader of this e-learning platform.Hope that you can have a nice experience with our platform.</p>
+                        <p>I'm a curious and motivated individual who enjoys learning new things and solving problems creatively.</p>
                     </div>
                     <div class="intro_image"><img src="img_logo/kwk.photo.jpg"></div>
                 </div>
